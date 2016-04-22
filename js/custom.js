4b1d7ac4a73192f2631951e16a1627f52575d510
Marcus Tran <marcus@hoiio.com>
2016-04-22 17:30:16 +0700

    update

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,8 @@
+$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	})
+
 $('.buttons a').on('click',function(e) {
     if ($(this).hasClass('grid')) {
         $('.container ul').removeClass('list').addClass('grid');
@@ -18,3 +23,28 @@ if ($('#content ul').hasClass('grid')){
 else if ($('#content ul').hasClass('list')){
     $('.buttons a.list').addClass('active');
 }
+
+var body = document.body,
+        overlay = document.querySelector('.overlay'),
+        overlayBtts = document.querySelectorAll('button[class$="overlay"]');
+        
+    [].forEach.call(overlayBtts, function(btt) {
+
+      btt.addEventListener('click', function() { 
+         
+         /* Detect the button class name */
+         var overlayOpen = this.className === 'open-overlay';
+         
+         /* Toggle the aria-hidden state on the overlay and the 
+            no-scroll class on the body */
+         overlay.setAttribute('aria-hidden', !overlayOpen);
+         body.classList.toggle('noscroll', overlayOpen);
+         
+         /* On some mobile browser when the overlay was previously
+            opened and scrolled, if you open it again it doesn't 
+            reset its scrollTop property */
+         overlay.scrollTop = 0;
+
+      }, false);
+
+    });
